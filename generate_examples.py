20b48e7fe069e62c9d566ac7fa5a638550f8898c
YuxiXie <xieyuxi@pku.edu.cn>
2021-10-03 14:52:00 +0800

    update

--- generate_examples.py
+++ generate_examples.py
@@ -42,7 +42,7 @@ def srl_process(srl):
     for k, v in srl.items():
         text, desc = v['text'], v['desc']
         color = COLOR_SRL[k]
-        txt_cmd = f'<font color={color} size="6">{text}</font>'
+        txt_cmd = f'<font color={color} size="5">{text}</font>'
         if desc and len(desc) > 2:
             txt_cmd += f' <font size="4">{desc}</font>'
         spans.append('<u>' + txt_cmd + '</u>')
